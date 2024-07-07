d2580689ebf0f3b5ee87c83e06eea7e2d804b794
Vivienne Drongowski <vividrongowski@gmail.com>
2024-07-07 17:45:03 +0200

    Added: Info Icon with text to the chart panels

---
+++ Assets/Scripts/UIElements/InfoIcon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UIElements
+{
+    public class InfoIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        public Tooltip tooltip;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            tooltip.ShowTooltip();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            tooltip.HideTooltip();
+        }
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (tooltip.IsActive)
+            {
+                tooltip.HideTooltip();
+            }
+            else
+            {
+                tooltip.ShowTooltip();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIElements/Tooltip.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace UIElements
+{
+    public class Tooltip : MonoBehaviour
+    {
+        public bool IsActive => gameObject.activeSelf;
+        
+        private void Start()
+        {
+            HideTooltip();
+        }
+        
+        public void ShowTooltip()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void HideTooltip()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
